6910955a091cf44e77ae3b9904d551f6cabf711e
Nik Rowe <nikrowedev@gmail.com>
2020-11-18 22:17:35 -0800

    Header & Site Components

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -1,12 +1,16 @@
 import headshot from "./images/headshot.jpg";
 import heroBackground from "./images/hero-background.jpg";
 import "./App.scss";
-import Nav from "./components/header";
+import Nav from "./components/nav";
+import Header from "./components/header";
+import Site from "./components/site";
 
 function App() {
   return (
     <div className="app">
       <Nav />
+      <Header imgSrc={heroBackground} />
+      <Site imgSrc={headshot} />
     </div>
   );
 }
