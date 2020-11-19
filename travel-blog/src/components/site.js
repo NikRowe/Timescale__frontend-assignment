1fa84d4f61540f281c7597a41e883991ca88accc
Nik Rowe <nikrowedev@gmail.com>
2020-11-18 22:18:02 -0800

    site

---
+++ travel-blog/src/components/site.js
@@ -0,0 +1,90 @@
+const Site = ({ imgSrc }) => (
+  <div className="site">
+    <img className="site__img site__img--round" src={imgSrc} alt="headshot" />
+    <div className="site__content">
+      <h1 className="site__header">A little about me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac
+        turpis egestas integer. In nisl nisi scelerisque eu ultrices vitae
+        auctor. Orci phasellus egestas tellus rutrum tellus pellentesque eu
+        tincidunt tortor. Placerat in egestas erat imperdiet sed. Molestie at
+        elementum eu facilisis. Duis at consectetur lorem donec massa sapien
+        faucibus et. Turpis cursus in hac habitasse platea dictumst. At quis
+        risus sed vulputate odio ut enim. Scelerisque mauris pellentesque
+        pulvinar pellentesque habitant morbi tristique. Parturient montes
+        nascetur ridiculus mus mauris vitae ultricies leo. Amet consectetur
+        adipiscing elit pellentesque habitant morbi. Pulvinar etiam non quam
+        lacus suspendisse faucibus interdum posuere lorem.
+      </p>
+      <p>
+        Vivamus at augue eget arcu dictum. Morbi quis commodo odio aenean sed
+        adipiscing. Tortor at auctor urna nunc id cursus. Viverra ipsum nunc
+        aliquet bibendum. Diam volutpat commodo sed egestas egestas fringilla
+        phasellus faucibus scelerisque. Sit amet cursus sit amet dictum sit amet
+        justo. Etiam sit amet nisl purus in mollis. Morbi tristique senectus et
+        netus et malesuada. Et odio pellentesque diam volutpat commodo. Id diam
+        maecenas ultricies mi. Dictumst vestibulum rhoncus est pellentesque
+        elit. Purus semper eget duis at. Posuere ac ut consequat semper viverra
+        nam libero. Lorem ipsum dolor sit amet consectetur adipiscing elit
+        pellentesque habitant. Mi bibendum neque egestas congue quisque.
+      </p>
+      <p>
+        Fringilla est ullamcorper eget nulla. Iaculis nunc sed augue lacus.
+        Velit scelerisque in dictum non consectetur. Sed risus pretium quam
+        vulputate dignissim suspendisse in. Consectetur libero id faucibus nisl
+        tincidunt. Scelerisque fermentum dui faucibus in ornare quam. Egestas
+        congue quisque egestas diam in arcu cursus euismod quis. Amet
+        consectetur adipiscing elit pellentesque habitant morbi tristique
+        senectus. Consectetur adipiscing elit pellentesque habitant morbi. Quam
+        vulputate dignissim suspendisse in est ante in nibh mauris. Lobortis
+        feugiat vivamus at augue eget arcu dictum. Pellentesque massa placerat
+        duis ultricies lacus sed turpis tincidunt. Sit amet venenatis urna
+        cursus eget. Dui accumsan sit amet nulla facilisi morbi tempus. Ultrices
+        eros in cursus turpis massa tincidunt dui ut. Fermentum odio eu feugiat
+        pretium. Adipiscing at in tellus integer feugiat scelerisque varius
+        morbi. Aliquet eget sit amet tellus cras. Amet venenatis urna cursus
+        eget nunc scelerisque viverra mauris in.
+      </p>
+      <p>
+        Sem et tortor consequat id porta nibh venenatis cras. Phasellus
+        vestibulum lorem sed risus. Sollicitudin ac orci phasellus egestas
+        tellus rutrum tellus pellentesque. Cum sociis natoque penatibus et
+        magnis dis. Nunc mattis enim ut tellus elementum sagittis. Diam donec
+        adipiscing tristique risus nec feugiat in. Vel turpis nunc eget lorem
+        dolor sed viverra ipsum nunc. Imperdiet dui accumsan sit amet nulla.
+        Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Mus
+        mauris vitae ultricies leo.
+      </p>
+      <p>
+        Donec adipiscing tristique risus nec feugiat in fermentum posuere.
+        Pellentesque elit eget gravida cum. Interdum consectetur libero id
+        faucibus nisl tincidunt. Sed vulputate mi sit amet mauris commodo quis
+        imperdiet. Erat pellentesque adipiscing commodo elit at imperdiet.
+        Malesuada fames ac turpis egestas sed tempus urna. Dignissim suspendisse
+        in est ante in nibh. Habitant morbi tristique senectus et. Et tortor
+        consequat id porta nibh venenatis cras sed. Lectus quam id leo in. Ut
+        tellus elementum sagittis vitae et leo. Dapibus ultrices in iaculis nunc
+        sed augue lacus. Dui sapien eget mi proin.
+      </p>
+      <p>
+        Scelerisque purus semper eget duis at tellus at urna condimentum. Urna
+        nec tincidunt praesent semper. Orci eu lobortis elementum nibh tellus
+        molestie nunc non. Lectus quam id leo in vitae. Neque aliquam vestibulum
+        morbi blandit cursus. Elit eget gravida cum sociis natoque penatibus.
+        Neque ornare aenean euismod elementum nisi quis eleifend quam
+        adipiscing. Sit amet justo donec enim. Egestas sed tempus urna et. Vitae
+        ultricies leo integer malesuada nunc vel risus commodo viverra. Eu
+        consequat ac felis donec et. Erat nam at lectus urna duis convallis
+        convallis tellus. Eu sem integer vitae justo eget magna fermentum.
+        Pellentesque dignissim enim sit amet. Viverra aliquet eget sit amet
+        tellus cras. Ac placerat vestibulum lectus mauris ultrices eros. Augue
+        interdum velit euismod in pellentesque massa placerat. Vitae ultricies
+        leo integer malesuada nunc vel risus commodo viverra. Morbi tempus
+        iaculis urna id volutpat lacus laoreet.
+      </p>
+    </div>
+  </div>
+);
+
+export default Site;
