7b5d62249c5e5494df3b85715a1eabebaab87d45
Nik Rowe <nikrowedev@gmail.com>
2020-11-18 22:17:44 -0800

    Header

--- travel-blog/src/components/header.js
+++ travel-blog/src/components/header.js
@@ -1,21 +1,5 @@
-const Nav = () => {
-  return (
-    <div className="nav">
-      <div className="nav__logo">
-        <h1 className="nav__logo__header nav__logo__header--skinny">
-          Travelize
-        </h1>
-        <p className="nav__logo__text">
-          <i>My traveling experiences</i>
-        </p>
-      </div>
-      <ul className="menu">
-        <li className="menu__item">Home</li>
-        <li className="menu__item">Blog</li>
-        <li className="menu__item">About</li>
-      </ul>
-    </div>
-  );
-};
+const Header = ({ imgSrc }) => (
+  <img src={imgSrc} alt="heroImage" className="heroImage" />
+);
 
-export default Nav;
+export default Header;
