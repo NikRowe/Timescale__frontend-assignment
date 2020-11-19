ba3cbbe7bf8320da2370ac3162a32aab60400636
Nik Rowe <nikrowedev@gmail.com>
2020-11-18 23:08:42 -0800

    Footer Component

---
+++ travel-blog/src/components/footer.js
@@ -0,0 +1,15 @@
+const Footer = () => (
+  <div className="footer">
+    <p>© 2020 Travelize</p>
+    <div className="links">
+      <a className="links__footer" href="/">
+        Privacy Policy
+      </a>
+      <a className="links__footer" href="/">
+        Terms and Conditions
+      </a>
+    </div>
+  </div>
+);
+
+export default Footer;
